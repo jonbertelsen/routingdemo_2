95e3d3fb3c86fe64f6a15114d38d0ea6d9a85cfd
Jon Bertelsen <jobe@cphbusiness.dk>
2024-11-26 23:18:02 +0100

    back to createRoutesFromElements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { Route, Routes, BrowserRouter } from 'react-router'
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router'
 import MainLayout from './layouts/MainLayout'
 import Products from './pages/Products'
 import Home from './pages/Home'
@@ -8,18 +8,18 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path='/products' element={<Products />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Route>
+    )
   )
+
+  return <RouterProvider router={router} />
 }
 
 export default App
