c93e209f0de5dc57d8bb4772131f35f8afa05edc
Jon Bertelsen <jobe@cphbusiness.dk>
2024-11-26 15:29:32 +0100

    add index

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router'
 import MainLayout from './layouts/MainLayout'
 import Products from './pages/Products'
+import Home from './pages/Home'
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
+        <Route index element={<Home />} />
         <Route path='/products' element={<Products />} />
       </Route>
     )
